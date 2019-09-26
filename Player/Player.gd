ca1916f9dd2f5b959f5898825a9079995dad689d
Artit <zunnieartit26@gmail.com>
2019-09-26 12:21:53 +0700

    animated+sound

--- Player/Player.gd
+++ Player/Player.gd
@@ -14,7 +14,25 @@ func _physics_process(delta):
 	apply_gravity()
 	walk()
 	jump()
-
+	
+	update_animation()
+	
+func die():
+	if position.y > 1000:
+		return true
+	return false
+	
+func update_animation():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x >0 and is_on_floor():
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
+	elif motion.x <0 and is_on_floor():
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
 	
 func walk():
 	if Input.is_action_pressed("left") and not Input.is_action_just_pressed("right"):
@@ -29,6 +47,7 @@ func jump():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -jump
 			jump_count = jump_count + 1
+			$AudioStreamPlayer.play()
 	if is_on_floor():
 		jump_count = 0
 
